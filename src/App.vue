982b56dbb9ab418d0a85eb2ebeb1abb1c763aae1
fuheideMayuyu <xiaguangding@163.com>
2021-06-06 22:32:52 +0800

    feat:项目初始化

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>慕课乐高标准版</h1>
+ <ul>
+   <li>开箱即用</li>
+   <li>支持ts</li>
+ </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default  defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
